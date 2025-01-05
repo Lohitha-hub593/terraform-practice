42c489962b5259b1580cec4236def46bb842fa3b
Lohitha-hub593 <yadavallilohitha98@gmail.com>
2025-01-05 18:35:14 +0530

    day3

---
+++ day-3-statefile/variables.tf
@@ -0,0 +1,18 @@
+variable "ami" {
+  description = "instering ami value"
+  type        = string
+  default     = ""
+
+}
+
+variable "type" {
+  type    = string
+  default = ""
+
+}
+variable "key" {
+  description = "inserting instance key value"
+  type        = string
+  default     = ""
+
+}
