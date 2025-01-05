13a2ed97acd78dc1a49f61c710702cb7e095a6d1
Lohitha-hub593 <yadavallilohitha98@gmail.com>
2025-01-05 19:38:22 +0530

    deleted var and tfvars

--- day-3-statefile/main.tf
+++ day-3-statefile/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "name" {
-  ami           = ami-0fd05997b4dff7aac
-  instance_type = t2.micro
-  key_name      = linuxkey
-  availability_zone = ap-south-1a
+  ami           = "ami-0fd05997b4dff7aac"
+  instance_type = "t2.micro"
+  key_name      = "linuxkey"
+  availability_zone = "ap-south-1a"
 }
--- day-3-statefile/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-ami  = "ami-0fd05997b4dff7aac"
-type = "t2.micro"
-key  = "linuxkey"
